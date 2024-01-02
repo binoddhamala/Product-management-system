4ee3396de87ae1bb095a13f7568b7874613bc893
Binod Dhamala <binoddhamala969@gmail.com>
2024-01-02 14:43:45 +0545

    add fetch products

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,32 @@
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Home.css'
 import Navbar from './Components/Navbar/Navbar'
+import axios from 'axios'
 
 const Home = () => {
+  const [products,setProducts ] = useState([])
+  const fetchProducts = async ()=>{
+    const response = await axios.get("https://6582de5c02f747c8367a6e08.mockapi.io/products")
+    setProducts(response.data)
+  }
+  useEffect(()=>{
+    fetchProducts()
+  },[])
   return (
 <>
     <Navbar/>
- <div className="card">
-    <img src="your-product-image.jpg" alt="Product Image"/>
-        <h2 className="product-name">Product Name</h2>
-        <p className="product-description">Product Description goes here. Provide a brief overview of the product.</p>
- </div>
+    {
+            products.map((product)=>{
+              return(
+        <div key={product.id} className="card">
+           <img src={product.productImage} alt="Product Image"/>
+           <h2 className="product-name">{product.productName}</h2>
+           <p className="product-description">{product.productDescription}</p>
+        </div>
+              )
+      })
+    }
 </>
   )
 }
